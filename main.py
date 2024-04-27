9d64a65e8d977b9b3c36c83a4ff5592de82af9aa
SEASIMG4 <seasigamer2010@gmail.com>
2024-04-28 00:25:55 +0400

    added main.py

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-from turtle
-g
+import turtle
+import keyboard
+
+turt = turtle.Turtle()
+
+while True:
+    if keyboard.is_pressed("w"):
+        turt.forward(20)
+    if keyboard.is_pressed("s"):
+        turt.back(20)
+    if keyboard.is_pressed("d"):
+        turt.right(90)
+    if keyboard.is_pressed("a"): 
+        turt.left(90)
+    if keyboard.is_pressed("esc"):
+        break
+    if keyboard.is_pressed("f"):
+        turt.color("red")
+        turt.forward(100)
+        turt.right(90)
+        turt.forward(100)
+        turt.left(90)
+        turt.forward(100)
+        turt.left(180)
+        turt.forward(100)
+        turt.right(90)
+        turt.forward(50)
+        turt.left(90)
+        turt.forward(100)
+        turt.left(90)
+        turt.forward(50)
+        turt.left(180)
+        turt.forward(50)
+        turt.right(90)
+        turt.forward(200)
+        turt.left(90)
+        turt.forward(50)
+        turt.right(90)
